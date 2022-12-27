1fe7e66a52eaa782c68368ce129212ec4a9f2d3a
matias <matifandy@gmail.com>
2022-12-26 21:35:11 -0300

    add icon play

--- components/episodeCard/index.tsx
+++ components/episodeCard/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import styles from '../../styles/Episodes.module.scss'
+import Link from 'next/link'
 
 
 
@@ -9,10 +10,13 @@ const EpisodeCard: React.FC<any> = ({ episodeItem }: any) => {
     return (
         <div key={episodeItem.id} className={styles.card}>
             <div className={styles.image}>
-                            <Image src={episodeItem.image} alt={episodeItem.name}  width={200} height={150} />
-                  <div className={styles.middle}>
-                    <p className={styles.text}>text</p>
-                  </div>
+                <Image src={episodeItem.image} alt={episodeItem.name} width={200} height={150} />
+                <div className={styles.middle}>
+                    <Link href="/episodes">
+                        <p className={styles.text}></p>
+
+                    </Link>
+                </div>
             </div>
             <h2>{episodeItem.id}: {episodeItem.name}</h2>
             <p>{episodeItem.date}</p>
