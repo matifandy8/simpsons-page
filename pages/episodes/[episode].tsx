cd75dba7ad420f5013753e37cd512f24e1585da5
matias <matifandy@gmail.com>
2023-02-11 17:42:50 -0300

    add details commentsection

--- pages/episodes/[episode].tsx
+++ pages/episodes/[episode].tsx
@@ -6,7 +6,20 @@ import { collection, getDocs } from 'firebase/firestore'
 import { db } from '../../firebase'
 
 
+
 const Episode = ({ episode, comments }: any) => {
+  const capitalize = (str: string) => str.replace(/^\w/, c => c.toUpperCase());
+
+  function getHumanReadableDate(dateString: string) {
+    const date = new Date(dateString);
+    const options:any = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    return date.toLocaleDateString("en-US", options);
+  }
+
   return (
     <div className={styles.episode}>
       <div className={styles.info}>
@@ -23,10 +36,14 @@ const Episode = ({ episode, comments }: any) => {
           {comments.map((comment: any) => (
             <div key={comment.timestamp} className={styles.comment}>
               <div className={styles.commentInfo}>
-                <p>{comment.name}</p>
-                <span className={styles.commentDate} >{comment.timestamp}</span>
+                <div className={styles.commentImage}>
+                  <img src="https://t4.ftcdn.net/jpg/03/59/58/91/360_F_359589186_JDLl8dIWoBNf1iqEkHxhUeeOulx0wOC5.jpg" alt="avatar" />
+                </div>
+                <p>{capitalize(comment.name)}</p>
+                <span className={styles.commentDate} >{getHumanReadableDate(comment.timestamp)}</span>
               </div>
               <p className={styles.commentText} >{comment.commentText}</p>
+              <hr />
             </div>
           ))}
         </div>
@@ -37,7 +54,7 @@ const Episode = ({ episode, comments }: any) => {
 
 export const getServerSideProps = async (pageContext: any) => {
   const pathurl = pageContext.params;
-  let comments:any = [];
+  let comments: any = [];
 
   const querySnapshot = await getDocs(collection(db, "comments"));
   querySnapshot.forEach((doc) => {
