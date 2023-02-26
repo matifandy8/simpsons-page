5b9eb3373152e648fe9ca569c5024e113a5345be
matias <matifandy@gmail.com>
2023-02-25 21:38:09 -0300

    add error messages and fix env

--- components/commentForm/index.tsx
+++ components/commentForm/index.tsx
@@ -8,12 +8,20 @@ import { db } from '../../firebase';
 const CommentForm: React.FC = () => {
     const [commentText, setCommentText] = useState("")
     const [name, setName] = useState("")
+    const [errorMessage, setErrorMessage] = useState('');
+    const [successMessage, setSuccessMessage] = useState('');
+
+
 
 
     const submitComment = async (e: React.SyntheticEvent) => {
         e.preventDefault();
         const now = new Date();
         const timestamp = now.toISOString();
+        if (!name || !commentText) {
+            setErrorMessage("Please fill in all fields");
+            return;
+        }
         try {
             const docRef = await addDoc(collection(db, "comments"), {
                 name: name,
@@ -21,8 +29,10 @@ const CommentForm: React.FC = () => {
                 timestamp: timestamp
             });
             console.log("Document written with ID: ", docRef.id);
+            setSuccessMessage("Comment is posted")
         } catch (e) {
             console.error("Error adding document: ", e);
+            setErrorMessage('Error adding comment. Please try again later.');
         }
         setCommentText("")
         setName("")
@@ -30,11 +40,13 @@ const CommentForm: React.FC = () => {
     return (
         <div className={styles.commentForm}>
             <h2>Comments</h2>
+            {errorMessage && <p className={styles.commentError}>{errorMessage}</p>}
+            {successMessage && <p className={styles.commentSuccess}>{successMessage}</p>}
             <form onSubmit={submitComment}>
                 <label>Name</label>
-                <input type="text" placeholder='Your name' value={name} onChange={(e) => setName(e.target.value)} />
+                <input type="text" placeholder='Your name' value={name} onChange={(e) => setName(e.target.value)} className={name === '' ? styles.empty : ''}/>
                 <label>Comment</label>
-                <textarea value={commentText} onChange={(e) => setCommentText(e.target.value)} />
+                <textarea value={commentText} onChange={(e) => setCommentText(e.target.value)} className={commentText === '' ? styles.empty : ''}/>
                 <button className={styles.btn}  type="submit">Post</button>
             </form>
 
